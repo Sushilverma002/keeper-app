5c95cecdaea86867f7544eea466c46a9dd285651
Sushilverma002 <114687502+Sushilverma002@users.noreply.github.com>
2024-04-24 07:27:46 +0530

    24april

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import List from "./List";
+import notes from "./Note";
 
 function App() {
   return (
     <>
       <Heading />
-      <List />
+      {/* {notes.map((cards) => {
+        <List key={cards.key} title={cards.title} head={cards.content} />
+      })} */}
+      {notes.map((cards) => (
+        <List key={cards.key} title={cards.title} head={cards.content} />
+      ))}
       <Footer />
     </>
   );
